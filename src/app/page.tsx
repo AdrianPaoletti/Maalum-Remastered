674e016e9fe7c192fc79f8741a74c8b335db4447
AdrianPaoletti <adrian.paoletti@bideaavant.com>
2023-08-04 12:37:09 +0200

    znz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,36 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+
+import { Navbar } from "maalum/components/Navbar/Navbar";
+
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+    <main>
+      <header className={styles["header"]}>
+        <Navbar />
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/images/header-cave.jpg"
+          alt="maalum-swimming-cave"
+          fill
+          style={{
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1 className={`${styles["header__title"]} heading-primary`}>
+          WELCOME TO PARADISE
+        </h1>
+      </header>
+      <section className="main-content">
+        <article
+          className={`${styles.description} u-padding-vertical-45 u-padding-horizontal-30`}
         >
-          <h2>
-            Docs <span>-&gt;</span>
+          <h2 className={`${styles["description__title"]} heading-secundary`}>
+            TAKE A BREATH <span>LOVE NATURE</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        </article>
+      </section>
     </main>
-  )
+  );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./Navbar.module.scss";
+
+interface NavbarProps {}
+
+export function Navbar({}: NavbarProps) {
+  const [hasScrollMoved, setHasScrollMoved] = useState<boolean>(false);
+
+  const handleScrollEvent = useCallback(() => {
+    if (window.scrollY > 0) {
+      setHasScrollMoved(true);
+      return;
+    }
+    setHasScrollMoved(false);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollEvent);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollEvent);
+    };
+  }, [handleScrollEvent]);
+
+  const menuItems: { id: string; title: string; refference?: string }[] = [
+    { id: "contact", title: "CONTACT US", refference: "contact" },
+    { id: "bookNow", title: "BOOK NOW" },
+  ];
+
+  return (
+    <nav
+      className={`${styles.navbar} ${
+        hasScrollMoved && styles["navbar--sticky"]
+      } u-padding-horizontal-30 u-padding-vertical-20`}
+    >
+      <div className={"col-1-of-2"}>
+        <Image
+          src="/images/header-logo.png"
+          alt="maalum-zanzibar-logo"
+          width={188}
+          height={46}
+        />
+      </div>
+      <div className={`${styles.menu} col-1-of-2`}>
+        <ul className={styles["menu__list"]}>
+          {menuItems.map(({ id, title, refference }) => (
+            <li
+              key={id}
+              className={`navbar-list-item ${styles["menu__item"]} ${
+                hasScrollMoved && styles["menu__item--sticky"]
+              }`}
+            >
+              {refference ? (
+                <Link
+                  href={`/#${refference}`}
+                  className={`${styles["menu__link-item"]}`}
+                >
+                  {title}
+                </Link>
+              ) : (
+                title
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
