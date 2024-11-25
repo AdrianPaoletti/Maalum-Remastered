1b453131d4eb041971b3aaa82c3789bbd9d1cb36
Adrian Paoletti <adriandelapao@gmail.com>
2024-11-25 18:38:23 +0100

    main

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+    // webpack: (config) => {
+    //     config.module.rules.push({
+    //         test: /\.svg$/,
+    //         use: ["@svgr/webpack"],
+    //     });
 
-    return config;
-  },
+    //     return config;
+    // },
+    output: "export",
 };
 
 module.exports = nextConfig;
