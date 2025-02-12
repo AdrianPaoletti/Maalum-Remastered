14080b0ed4da400f4642a5e656eb725014f63bad
Adrian Paoletti <adriandelapao@gmail.com>
2025-02-12 10:09:37 +0100

    m

--- src/core/constants/constants.ts
+++ src/core/constants/constants.ts
@@ -6,11 +6,11 @@ export const CHILDREN_PRICE = 10;
 export const RESIDENTS_PRICE = 15;
 export const MAX_GUESTS_PER_HOUR = 10;
 export const MAX_DISABLE_CAVE = 7;
-export const NATURAL_ESSENCE_PRICE_PROMO = 60;
+export const NATURAL_ESSENCE_PRICE_PROMO = 65;
 export const NATURAL_ESSENCE_PRICE = 65;
 export const RITUAL_PRICE_PROMO = 80;
 export const RITUAL_PRICE = 85;
 export const WHATSAPP_LINK =
-  "https://wa.me/255772628924?text=Hi%20Maalum,%20I%20would%20like%20...";
+    "https://wa.me/255772628924?text=Hi%20Maalum,%20I%20would%20like%20...";
 
 export const EMAIL_REGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
